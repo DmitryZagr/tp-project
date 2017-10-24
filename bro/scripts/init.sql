f8362eb951536217520cc033dc19fd06c1acc383
TaxaRun <taxarun@gmail.com>
2017-10-24 20:33:07 +0300

    Modifyed bash scripts to load non merged files to HDFS, also now they show their work status. Added new SQL examples for work with external tables

--- bro/scripts/init.sql
+++ bro/scripts/init.sql
@@ -1,7 +1,9 @@
 
 CREATE DATABASE IF NOT EXISTS BRO;
 
-CREATE TABLE IF NOT EXISTS BRO.NOTICE (
+-- EXTERNAL allows us not to copy data in hive location
+-- location should be defined (raw logs in HDFS)
+CREATE EXTERNAL TABLE IF NOT EXISTS BRO.NOTICE (
                                         ts BIGINT COMMENT 'Timstamp', uid STRING COMMENT 'Connection unique id',
                                         id_orig_h STRING COMMENT 'Originating endpoint’s IP address (AKA ORIG)',
                                         id_orig_p INT COMMENT 'Originating endpoint’s TCP/UDP port (or ICMP code)',
@@ -22,17 +24,40 @@ CREATE TABLE IF NOT EXISTS BRO.NOTICE (
                                         suppress_for BIGINT COMMENT 'Length of time dupes should be suppressed',
                                         dropped STRING COMMENT 'If the src IP was blocked'
                                       )
-ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t" STORED AS TEXTFILE 
+ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t" STORED AS TEXTFILE LOCATION "/user/hive/rawlogs/notice"
+
+-- This command should be executed for ANY table using external HDFS data,
+-- because log files could be located in subdirectories
+-- WITHOUT this command hive WON'T search subdirs!!!!!!!!!!!
+alter table [TABLE_NAME] set tblproperties ("hive.input.dir.recursive" = "TRUE",
+     "hive.mapred.supports.subdirectories" = "TRUE",
+     "hive.supports.subdirectories" = "TRUE",
+     "mapred.input.dir.recursive" = "TRUE"
+     )
 
 
-
-
-CREATE TABLE IF NOT EXISTS BRO.CONN (duration DOUBLE, service STRING, Source_bytes INT,
-								 Destination_bytes INT, count INT, Same_srv_rate FLOAT, Serror_rate FLOAT, 
-								 Srv_serror_rate FLOAT, Dst_host_count INT, Dst_host_srv_count INT, 
-								 Dst_host_same_src_port_rate FLOAT, Dst_host_serror_rate FLOAT, 
-								 Dst_host_srv_serror_rate FLOAT, Flag STRING, IDS_detection INT, 
-								 Malware_detection INT, Ashula_detection STRING, Label INT,
-								 Source_IP_Address STRING, Source_Port_Number INT, 
-							 	 Destination_IP_Address STRING, Destination_Port_Number INT, Start_Time STRING, 
-								 Duration_session STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY \"\t\" STORED AS TEXTFILE; 
+CREATE EXTERNAL TABLE IF NOT EXISTS BRO.CONNECTION (
+                                        ts DOUBLE COMMENT 'Timstamp', ---TODO: fix timestamp type
+                                        uid STRING COMMENT 'Unique ID of Connection',
+                                        id_orig_h STRING COMMENT 'Originating endpoint’s IP address (AKA ORIG)',
+                                        id_orig_p INT COMMENT 'Originating endpoint’s TCP/UDP port (or ICMP code)',
+                                        id_resp_h STRING COMMENT 'Responding endpoint’s IP address (AKA RESP)',
+                                        id_resp_p INT COMMENT 'Responding endpoint’s TCP/UDP port (or ICMP code)',
+                                        proto STRING COMMENT 'Transport layer protocol of connection',
+                                        service STRING COMMENT 'Dynamically detected application protocol, if any',
+                                        duration DOUBLE COMMENT 'Time of last packet seen – time of first packet seen',
+                                        orig_bytes INT COMMENT 'Originator payload bytes; from sequence numbers if TCP',
+                                        resp_bytes INT COMMENT 'Responder payload bytes; from sequence numbers if TCP',
+                                        conn_state STRING COMMENT 'Connection state (see conn.log:conn_state table)', ---all clear
+                                        local_orig STRING COMMENT 'If conn originated locally T; if remotely F. If Site::local_nets empty, always unset.', ---unknown
+                                        missed_bytes INT COMMENT 'Number of missing bytes in content gaps',
+                                        history STRING COMMENT 'Connection state history (see conn.log:history table)',
+                                        orig_pkts INT COMMENT 'Number of ORIG packets',
+                                        orig_ip_bytes INT COMMENT 'Number of ORIG IP bytes (via IP total_length header field)',
+                                        resp_pkts INT COMMENT 'Number of RESP packets',
+                                        resp_ip_bytes INT COMMENT 'Number of RESP IP bytes (via IP total_length header field)',
+                                        tunnel_parents STRING COMMENT 'If tunneled, connection UID of encapsulating parent (s)',
+                                        orig_cc STRING COMMENT 'ORIG GeoIP Country Code',
+                                        resp_cc STRING COMMENT 'RESP GeoIP Country Code'
+                                      )
+ROW FORMAT DELIMITED FIELDS TERMINATED BY "\t" STORED AS TEXTFILE LOCATION "/user/hive/rawlogs/conn" 
--- commons/bash/README.md
+++ commons/bash/README.md
@@ -1,26 +1,31 @@
 # Bash scripts
 
+## NOTICE: this script versions load unarchived data to HDFS WITHOUT merging
+## In hive you should use EXTERNAL tables, see examples in bro/scripts/init.sql
+
 ## Scripts to load data in HDFS:
 
 ### Internal script (runs in datanode's docker container)
 
+Should be located in /bin/ of datanode's docker container
+
 ```sh
 $ loadArchiveToHDFS [path to any format archive]
 ```
-Script will create merged file with the content of the archive at HDFS path: 
-"/user/hive/rawlogs".
-Supports .txt, .log and .csv files to be in the archives.
-File will be named as the archive and will be of its content format.
-For example, we have archive 200611.zip and it contains files of .txt format.
-Than we'll have a file in HDFS named 200611.txt.
-
-SHOULD BE PLACED IN /bin/ !!! (in datanode's docker container)
+Script will extract files from archive and than put them at HDFS path: 
+"/user/hive/rawlogs/[folder of the name of the archive]".
+For example, we have archive 200611.zip.
+Than we'll have extracted files at HDFS path:
+/user/hive/rawlogs/200611
 
 ### Local machine script (runs out of docker container, but needs the container to be running)
 
-Just call the script from its folder:
+Just call the script from 'archives' folder, that is located
+in 'docker-spark-hive-zeppelin' folder
 
 ```sh
 $ extractArchiveInHDFS
 ```
+
+The script will read all the archives you've added to 'archives'
 Then you'll be guided through some sort of interface to extract your archive and make it to be ready for Hadoop proccessing 
